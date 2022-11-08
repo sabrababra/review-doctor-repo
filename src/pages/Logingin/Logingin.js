693688e9dc19d00ff0ad7b66b25be2aee6cee957
sabrababra <sabrinayesminprome@gmail.com>
2022-11-08 14:20:57 +0600

    Signup interaction complete

--- src/pages/Logingin/Logingin.js
+++ src/pages/Logingin/Logingin.js
@@ -5,22 +5,22 @@ import { FaGithub, FaGoogle } from "react-icons/fa";
 const Logingin = () => {
     return (
         <div className="flex gap-5  min-h-screen bg-base-200  flex-col justify-center items-center">
- 
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-     
-    </div>
-    <div className="card w-11/12 lg:w-4/12  mx-auto  shadow-2xl bg-base-100">
-      <div className="card-body">
-       <form >
-       <div className="form-control">
+
+            <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Login now!</h1>
+
+            </div>
+            <div className="card w-11/12 lg:w-4/12  mx-auto  shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <form >
+                        <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="enter email" className="input input-bordered" name='email' required/>
+                            <input type="email" placeholder="enter email" className="input input-bordered" name='email' required />
                         </div>
 
-        <div className="form-control">
+                        <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
@@ -29,24 +29,24 @@ const Logingin = () => {
                                 <Link to='' className="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
                             <label className="label">
-                                <Link to='/register' className="label-text-alt link link-hover">Create an account</Link>
+                                <Link to='/signup' className="label-text-alt link link-hover">Create an account</Link>
                             </label>
 
                         </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
 
-       </form>
-       <div className="divider">OR</div>
-                    <div className="flex justify-evenly mt-6">
-                        <button  className=""><FaGoogle className=' text-2xl hover:text-primary '></FaGoogle></button>
-                        <button  className=""><FaGithub className=' text-2xl hover:text-primary'></FaGithub></button>
+                    </form>
+                    <div className="divider">OR</div>
+                    <div className=" flex m-0 p-0 ">
+                        <button className="mb-5"><FaGoogle className=' text-2xl hover:text-primary '></FaGoogle></button>
+                        <button className=""><FaGithub className=' text-2xl hover:text-primary'></FaGithub></button>
                     </div>
-      </div>
-    </div>
+                </div>
+            </div>
 
-</div>
+        </div>
     );
 };
 
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
+import {  toast } from 'react-toastify';
 
 const SignUp = () => {
+    const {createProviderWithEmail,updateUserProfile}=useContext(AuthContext);
+    const [error,setError]=useState('');
+    const handleRegister=(event)=>{
+        event.preventDefault();
+        const form=event.target;
+        const name=form.name.value;
+        const photo=form.photo.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        const confirm=form.confirm.value;
+        // console.log(name,photo,email,password,confirm);
+        if(password===confirm){
+            setError('');
+            createProviderWithEmail(email,password)
+            .then(res=>{
+                const user=res.user;
+                // console.log(user);
+                handleUpdateUserProfile(name,photo);
+                toast.success('Registration complete!');
+                form.reset();
+            })
+        }else{
+            setError('Password does not match with confirm password');
+        }
+
+        const handleUpdateUserProfile=(name,photoURL)=>{
+            const profile={
+                displayName: name,
+            photoURL: photoURL 
+            }
+            updateUserProfile(profile)
+            .then(()=>{})
+            .catch(error=>console.error(error));
+            
+        }
+    }
     return (
         <div className="flex gap-5  min-h-screen bg-base-200  flex-col justify-center items-center">
      
@@ -10,7 +48,7 @@ const SignUp = () => {
            
           </div>
           <div className="card w-11/12 lg:w-4/12  mx-auto  shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
             <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
@@ -63,6 +101,9 @@ const SignUp = () => {
                         </label>
 
                     </div>
+                    {
+                        error&&<p className='text-red-500'>{error}</p>
+                    }
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register</button>
               </div>
